324aff9b9ed912f33683092bffda6e260df7c048
Alban FALCOZ <alban.falcoz@orange.fr>
2023-12-22 11:49:38 +0100

    Début assembleur

---
+++ src/Assembler/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] lines = File.ReadAllLines(@"data_processing\1-4_instructions.s");
+foreach (string line in lines)
+{
+    System.Console.WriteLine(line);
+    if (line.Length == 0 || line[0] == '@') continue;
+    string[] lineSplit = line.Split(' ', 2);
+    string instruction = lineSplit[0];
+    string[] arguments = lineSplit[1].Split(',').Select(arg => arg.Trim()).ToArray();
+    System.Console.WriteLine(instruction);
+    foreach(var item in arguments)
+    {
+        Console.WriteLine(item.ToString());
+    }
+    
+}
---
+++ src/Assembler/SASM.cs
@@ -0,0 +1,27 @@
+﻿namespace Assembler;
+
+public class SASM
+{
+    const string opcodePrefix = "00";
+
+    public string getBinary(string instruction, string[] arguments)
+         => opcodePrefix + instruction switch
+        {
+            "LSLS" =>  "000" + concatArgs(arguments, 2,1,0),
+            "LSRS" =>  "001" + concatArgs(arguments, 2,1,0),
+            "ASRS" => "010" + concatArgs(arguments, 2,1,0),
+            "ADDS" => "01100" + concatArgs(arguments, 2,1,0),
+            "SUBS" => "01101" + concatArgs(arguments, 2,1,0),
+            //todo: duplication ADDS et SUBS
+        };
+
+    public string concatArgs(string[] arguments, params int[] indexes)
+    {
+        string endOfResult = "";
+        foreach (int index in indexes)
+        {
+            endOfResult += arguments[index];
+        }
+        return endOfResult;
+    }
+}
