2be5b1594dff9e09dfc623f58decedc985fb64da
Evan Galli <evan_g@orange.fr>
2024-02-03 16:13:07 +0100

    Load/Store no longer randomly modifies registries

--- code_c/buzzer_melody.c
+++ code_c/buzzer_melody.c
@@ -1,102 +1,78 @@
 #include <parm.h>
 #include <audio.h>
 
-#define WAIT 3
-#define WAIT2 0
-#define SLEEP(time) for (int i = 0; i < time; i++)
+#define SLEEP(times) for (int time = 0; time < times; time++)
 
 void run()
 {
 	BEGIN();
 
+	int notes[44];
+	int *t = notes;
+
+	// Au clair de la lune, mon amis Pierot
+	*(t++) = 262;
+	*(t++) = 262;
+	*(t++) = 262;
+	*(t++) = 294;
+	*(t++) = 330; // aurait du etre une blanche
+	*(t++) = 294; // aurait du etre une blanche
+	*(t++) = 262;
+	*(t++) = 330;
+	*(t++) = 294;
+	*(t++) = 294;
+	*(t++) = 262; // aurait du etre une blanche
+
+	// Prete moi ta plume, pour écrire un mot
+	*(t++) = 262;
+	*(t++) = 262;
+	*(t++) = 262;
+	*(t++) = 294;
+	*(t++) = 330; // aurait du etre une blanche
+	*(t++) = 294; // aurait du etre une blanche
+	*(t++) = 262;
+	*(t++) = 330;
+	*(t++) = 294;
+	*(t++) = 294;
+	*(t++) = 262; // aurait du etre une blanche
+
+	// Ma chandele est morte, je n'ai plus de feu
+	*(t++) = 294;
+	*(t++) = 294;
+	*(t++) = 294;
+	*(t++) = 294;
+	*(t++) = 220; // aurait du etre une blanche
+	*(t++) = 220; // aurait du etre une blanche
+	*(t++) = 294;
+	*(t++) = 262;
+	*(t++) = 246;
+	*(t++) = 220;
+	*(t++) = 196; // aurait du etre une blanche
+
+	// Ouvre moi ta porte, pour l'amour de dieu
+	*(t++) = 262;
+	*(t++) = 262;
+	*(t++) = 262;
+	*(t++) = 294;
+	*(t++) = 330; // aurait du etre une blanche
+	*(t++) = 294; // aurait du etre une blanche
+	*(t++) = 262;
+	*(t++) = 330;
+	*(t++) = 294;
+	*(t++) = 294;
+	*(t++) = 262; // aurait du etre une blanche
+
 	int vol = SLIDER << 9;
-	BZZSET(130, vol); // C3
-	SLEEP(WAIT);
-	BZZCLR();
-	SLEEP(WAIT2);
-	BZZSET(130, vol); // C3
-	SLEEP(WAIT);
-	BZZCLR();
-	SLEEP(WAIT2);
-	BZZSET(130, vol); // C3
-	SLEEP(WAIT);
-	BZZCLR();
-	SLEEP(WAIT2);
-	BZZSET(146, vol); // D3
-	SLEEP(WAIT);
-	BZZCLR();
-	SLEEP(WAIT2);
-	BZZSET(165, vol); // E3
-	SLEEP(WAIT);
-	BZZCLR();
-	SLEEP(WAIT2);
-	BZZSET(146, vol); // D3
-	SLEEP(WAIT);
-	BZZCLR();
-	SLEEP(WAIT2);
-	BZZSET(130, vol); // C3
-	SLEEP(WAIT);
-	BZZCLR();
-	SLEEP(WAIT2);
-	BZZSET(165, vol); // E3
-	SLEEP(WAIT);
-	BZZCLR();
-	SLEEP(WAIT2);
-	BZZSET(146, vol); // D3
-	SLEEP(WAIT);
-	BZZCLR();
-	SLEEP(WAIT2);
-	BZZSET(146, vol); // D3
-	SLEEP(WAIT);
-	BZZCLR();
-	SLEEP(WAIT2);
-	BZZSET(130, vol); // C3
-	SLEEP(WAIT);
-	BZZCLR();
-	SLEEP(WAIT2);
-	BZZSET(130, vol); // C3
-	SLEEP(WAIT);
-	BZZCLR();
-	SLEEP(WAIT2);
-	BZZSET(130, vol); // C3
-	SLEEP(WAIT);
-	BZZCLR();
-	SLEEP(WAIT2);
-	BZZSET(130, vol); // C3
-	SLEEP(WAIT);
-	BZZCLR();
-	SLEEP(WAIT2);
-	BZZSET(146, vol); // D3
-	SLEEP(WAIT);
-	BZZCLR();
-	SLEEP(WAIT2);
-	BZZSET(165, vol); // E3
-	SLEEP(WAIT);
-	BZZCLR();
-	SLEEP(WAIT2);
-	BZZSET(146, vol); // D3
-	SLEEP(WAIT);
-	BZZCLR();
-	SLEEP(WAIT2);
-	BZZSET(130, vol); // C3
-	SLEEP(WAIT);
-	BZZCLR();
-	SLEEP(WAIT2);
-	BZZSET(165, vol); // E3
-	SLEEP(WAIT);
-	BZZCLR();
-	SLEEP(WAIT2);
-	BZZSET(146, vol); // D3
-	SLEEP(WAIT);
-	BZZCLR();
-	SLEEP(WAIT2);
-	BZZSET(146, vol); // D3
-	SLEEP(WAIT);
-	BZZCLR();
-	SLEEP(WAIT2);
-	BZZSET(130, vol); // C3
-	SLEEP(WAIT);
-	BZZCLR();
+	while (1)
+	{
+		for (int i = 0; i < 44; i++)
+		{
+			BZZCLR();
+			BZZSET(notes[i], vol);
+			RES = i;
+			SLEEP(3);
+		}
+	}
 
 	END();
 }
